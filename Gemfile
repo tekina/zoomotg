6e07ff93b5a7e7a786dc625b864480eace1acd8d
Aniket Garg <aniketgarg@outlook.com>
2016-06-11 07:54:54 +0530

    search and stuffs

--- Gemfile
+++ Gemfile
@@ -29,6 +29,8 @@ gem 'spring',        group: :development
 
 gem 'zoomcar_otg', :git => "https://github.com/ahitendra/Zoomcar-OTG"
 
+gem 'pry'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/zoomcar_controller.rb
+++ app/controllers/zoomcar_controller.rb
@@ -1,16 +1,46 @@
 class ZoomcarController < ApplicationController
+
+  before_filter :get_auth_token, only: [:create]
+
+  @token = "this_is_the_token"
+
   def index
 
   end
 
   def search
-  	# render json: { status: params[:city], message: params[:pickup], message2: params[:dropoff] }
-  	token = "this_is_the_token"
-  	search = ZoomcarOtg::Search.new(token)
-  	results = 
-  	results = search.airport({ :city => params[:city], :starts => params[:pickup], :ends => params[:dropoff] })
-  	res = {"cars":[{"id":1,"seater":5,"name":"Figo","url":"http://s3-ap-southeast-1.amazonaws.com/zoomcar/pictures/original/3a1ab1fa8df989804714d57318c1cb42c7c1d213.jpg?-1425200817","brand":"Ford","pricing":[{"id":771,"amount":5185,"kms":365},{"id":772,"amount":6760,"kms":730},{"id":773,"amount":8930,"kms":1095}],"locations":[{"id":"17","starts":"2016-06-12 01:00","ends":"2016-06-15 01:30","pricing":[{"id":771,"amount":5185,"kms":365},{"id":772,"amount":6760,"kms":730},{"id":773,"amount":8930,"kms":1095}]}]}}
-  	render json: results
+    binding.pry
+    search = ZoomcarOtg::Search.new(@token)
+    results = search.airport({ :city => params[:city], :starts => params[:pickup], :ends => params[:dropoff] })
+    binding.pry
+    res = JSON.parse(results)
+    data = {}
+    res["result"][0]["cars"].each do |r|
+      data.merge!({ r["id"] => { location_id: r["locations"][0]["id"], 
+        carname: r["brand"] + " " + r["name"], starts: r["locations"][0]["starts"], ends: r["locations"][0]["ends"], 
+        city: params[:city], pricing_id: r["locations"][0]["pricing"][1]["id"], 
+        amount: r["locations"][0]["pricing"][1]["amount"], kms: r["locations"][0]["pricing"][1]["kms"]
+      } })
+    end
+    @data = data
+  end
+
+  def create
+    binding.pry
+    booking = ZoomcarOtg::Booking.new(@token)
+    bkg_data = booking.create({auth_token: @auth_token, city: params[:city], starts: params[:starts], ends: 
+      params[:ends], location_id: params[:location_id], cargroup_id: params[:cargroup_id], pricing_id: params[:pricing_id]
+      })
+    binding.pry
+    return bkg_data
+  end
+
+  protected
+
+  def get_auth_token
+    user = ZoomcarOtg::User.new(@token)
+    @auth_token = user.auth_token({:email => 'aniket.garg@zoomcar.com', :name => "Aniket Garg"})
+    return auth_token
   end
 
 end
